641e80ac4cde709cca3232f6af1908d202eac716
Rohan Roy <rohansroy@gmail.com>
2012-03-19 19:56:03 -0700

    Added UserProfile model, the model that contains information about user's websites, profile images, and other possible information to Django's admin menus and controllers.

--- booklistapp/admin.py
+++ booklistapp/admin.py
@@ -1,9 +1,11 @@
 from models import *
 from django.contrib import admin
+from django import forms
 
 admin.site.register(Book)
 admin.site.register(Category)
 admin.site.register(CategoryType)
 #admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(Recommendation)
 admin.site.register(FeedbackNote)
